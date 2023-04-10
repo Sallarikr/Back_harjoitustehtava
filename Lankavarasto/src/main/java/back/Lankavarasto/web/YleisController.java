7d3f49bc5f48cddf95773f67e4eedb13bfb80227
Salla Komppa <salla.halminen@hotmail.com>
2023-04-10 05:05:53 +0300

    Lisäyslomakkeiden lisäystä

--- Lankavarasto/src/main/java/back/Lankavarasto/web/YleisController.java
+++ Lankavarasto/src/main/java/back/Lankavarasto/web/YleisController.java
@@ -4,9 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import back.Lankavarasto.domain.Lanka;
 import back.Lankavarasto.domain.LankaRepository;
+import back.Lankavarasto.domain.Ohje;
 import back.Lankavarasto.domain.OhjeRepository;
+import back.Lankavarasto.domain.Vari;
 import back.Lankavarasto.domain.VariRepository;
 
 @Controller
@@ -25,11 +30,38 @@ public class YleisController {
 		return "index";
 	}
 	
+	
+	
 	@GetMapping("/lankalista")
 	public String naytaLangat(Model model) {
 		model.addAttribute("langat", lankaRepository.findAll());
 		return "lankalista";
 	}
+	
+	@GetMapping("/lisaaLanka")
+	public String lisaaLanka(Model model) {
+		model.addAttribute("lisaaUusiLanka", new Lanka());
+		return "lisaaLanka";
+	}
+	
+	@PostMapping("tallennaLanka")
+	public String tallennaLanka(Lanka lanka) {
+		lankaRepository.save(lanka);
+		return "redirect:/lankalista";
+	}
+	
+	@GetMapping("muokkaaLankaa/{id}")
+	public String muokkaaLankaa(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("muokkaaLankaa", lankaRepository.findAll());
+		return "muokkaaLankaa";
+	}
+	
+	@GetMapping("poistaLanka/{id}")
+	public String poistaLanka(@PathVariable("id") Long id) {
+		lankaRepository.deleteById(id);
+		return "redirect:/lankalista";
+	}
+
 	
 
 	@GetMapping("/ohjelista")
@@ -37,11 +69,65 @@ public class YleisController {
 		model.addAttribute("ohjeet", ohjeRepository.findAll());
 		return "ohjelista";
 	}
+
+	@GetMapping("/lisaaOhje")
+	public String lisaaOhje(Model model) {
+		model.addAttribute("lisaaUusiOhje", new Ohje());
+		return "lisaaOhje";
+	}
+	
+	@PostMapping("tallennaOhje")
+	public String tallennaOhje(Ohje ohje) {
+		ohjeRepository.save(ohje);
+		return "redirect:/ohjelista";
+	}
+	
+	@GetMapping("muokkaaOhjetta/{id}")
+	public String muokkaaOhjetta(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("muokkaaOhjetta", ohjeRepository.findAll());
+		return "muokkaaOhjetta";
+	}
+	
+	@GetMapping("poistaOhje/{id}")
+	public String poistaOhje(@PathVariable("id") Long id) {
+		ohjeRepository.deleteById(id);
+		return "redirect:/ohjelista";
+	}
 	
 
+	
 	@GetMapping("/varilista")
 	public String naytaVarit(Model model) {
 		model.addAttribute("varit", variRepository.findAll());
 		return "varilista";
 	}	
+
+	@GetMapping("/lisaaVari")
+	public String lisaaVari(Model model) {
+		model.addAttribute("lisaaUusiVari", new Vari());
+		return "lisaaVari";
+	}
+	
+	@PostMapping("tallennaVari")
+	public String tallennaVari(Vari vari) {
+		variRepository.save(vari);
+		return "redirect:/varilista";
+	}
+
+	@GetMapping("muokkaaVaria/{id}")
+	public String muokkaaVaria(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("muokkaaVaria", variRepository.findAll());
+		return "muokkaaVaria";
+	}
+	
+	@GetMapping("poistaVari/{id}")
+	public String poistaVari(@PathVariable("id") Long id) {
+		variRepository.deleteById(id);
+		return "redirect:/varilista";
+	}
+	
+	
+	
+	
+
 }
